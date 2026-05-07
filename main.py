f1b672c6097a65fef57b9d6f81870b0f7c8dd874
Adit Gautam <gautamadit@gmail.com>
2026-05-06 17:23:52 -0700

    fix

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import os
-import sys
-
 def main():
     unused_variable = 123
     print("Hello from a5-dsc190!")
-    print(  "bad spacing"  )
+    print("bad spacing")
+    print(unused_variable)
 
 
 if __name__ == "__main__":
